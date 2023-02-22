c430b7d09a8d984b8126f488ba1749c716d43114
YOURIxYOURI <97059905+YOURIxYOURI@users.noreply.github.com>
2023-02-22 10:20:14 +0100

    plik auto

---
+++ pliki_auta/pliki_auta/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pliki_auta
+{
+    internal class Samochod
+    {
+        public string marka { get; set; }
+        public string model { get; set; }
+        public double poj{ get; set; }
+        public int moc { get; set; }
+
+        public Samochod() { }
+        public Samochod(string marka, string model, double poj, int moc)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.poj = poj;
+            this.moc = moc;
+        }
+        public Samochod(Samochod s) 
+        {
+            this.marka = s.marka;
+            this.model = s.model;
+            this.poj = s.poj;
+            this.moc = s.moc;
+        }
+
+        public void Copy(Samochod s)
+        {
+            this.marka = s.marka;
+            this.model = s.model;
+            this.poj = s.poj;
+            this.moc = s.moc;
+        }
+
+        public override string ToString()
+        {
+            return $"{marka} {model} {poj} {moc}";
+        }
+    }
+}
---
+++ pliki_auta/pliki_auta/Program.cs
@@ -0,0 +1,33 @@
+﻿using pliki_auta;
+
+
+string[] tmp;
+string line;
+string path = @"C:\Users\admin\Desktop\qwerty\auta.txt";
+List<Samochod> samochody = new(); 
+try
+{
+    if (!File.Exists(path)) 
+    {
+        Console.WriteLine("SCIEZKA");
+    }
+    
+    using (StreamReader file = new StreamReader(path))
+    {
+        line = file.ReadLine();
+        while (line != null)
+        {
+            tmp = line.Split(' ');
+            samochody.Add(new Samochod(tmp[0], tmp[1], double.Parse(tmp[2], System.Globalization.CultureInfo.InvariantCulture), Convert.ToInt16(tmp[3])));
+            line = file.ReadLine();
+        }
+    }
+    foreach (Samochod i in samochody)
+        Console.WriteLine(i.ToString());
+}
+catch (Exception e)
+{
+    Console.WriteLine("The process failed: {0}", e.ToString());
+}
+
+
