ae8815a35363a85b884487b1efa5b0cf04fdd7a5
YOURIxYOURI <97059905+YOURIxYOURI@users.noreply.github.com>
2023-03-09 10:29:42 +0100

    first childsss

---
+++ first_class_child/first_class_child/Grade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_class_child
+{
+    internal class Grade
+    {
+        private string _SubjectName;
+        private DateTime _Date;
+        private double _Value;
+
+        public string SubjectName { get { return _SubjectName; } set { _SubjectName = value; } }
+        public DateTime Date { get { return _Date; } set { _Date = value; } }
+        public double Value { get { return _Value; } set { _Value = value; } }
+
+        public Grade() { }
+
+        public Grade(string subjectName, DateTime date, double value)
+        {
+            SubjectName = subjectName;
+            Date = date;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{_SubjectName} {_Date} {_Value}";
+        }
+        public void details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ first_class_child/first_class_child/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_class_child
+{
+    internal class Person
+    {
+        protected string _FirstName;
+        protected string _LastName;
+        protected DateTime _DateOfBirth;
+
+        public string FirstName { get { return _FirstName; } set { _FirstName = value; } }
+        public string LastName { get { return _LastName; } set { _LastName = value; } }
+        public DateTime DateOfBirth { get { return _DateOfBirth; } set { _DateOfBirth = value; } }
+
+        public Person() {}
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            _FirstName = firstName;
+            _LastName = lastName;
+            _DateOfBirth = dateOfBirth;
+        }
+        public override string ToString()
+        {
+            return $"{_FirstName} {_LastName} {_DateOfBirth} ";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
