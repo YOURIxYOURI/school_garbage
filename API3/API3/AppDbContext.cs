015451a729412206aefd1200723cb9a33331f9ae
YouriProgrammer <97059905+YOURIxYOURI@users.noreply.github.com>
2023-10-13 09:15:23 +0200

    Add files via upload

---
+++ API3/API3/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API3
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Ksiazka> Ksiazka { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var serverversion = new MySqlServerVersion(new Version(1,1,0));
+            optionsBuilder.UseMySql(_configuration.GetConnectionString("DBConnection"), serverversion).UseLazyLoadingProxies();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ API3/API3/Ksiazka.cs
@@ -0,0 +1,10 @@
+﻿namespace API3
+{
+    public class Ksiazka
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string Autor { get; set; }
+        public int Rok {  get; set; }
+    }
+}
